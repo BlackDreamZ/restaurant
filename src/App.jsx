2e56e3681ab422c368276b5c10c9c9563cee7724
Denis <kolesnichenko-den@mail.ru>
2025-11-01 22:53:35 +0300

    feat: 1 homework

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {restaurants} from './mock/mock.js';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+        {
+            React.createElement('div', {}, React.createElement('h1', {}, 'Yummy food'),
+                React.createElement('h3', {}, 'Menu'),
+                React.createElement('ul', {},
+                    React.createElement('li', null, restaurants[0].name),
+                    React.createElement('li', null, restaurants[1].name),
+                    React.createElement('li', null, restaurants[2].name),
+                    React.createElement('li', null, restaurants[3].name)
+                ),
+                React.createElement('h3', {}, 'List of review'),
+                React.createElement('ul', {},
+                    React.createElement('li', null, restaurants[0].reviews[0].text),
+                    React.createElement('li', null, restaurants[0].reviews[1].text),
+                    React.createElement('li', null, restaurants[1].reviews[0].text),
+                    React.createElement('li', null, restaurants[1].reviews[1].text),
+                    React.createElement('li', null, restaurants[1].reviews[2].text),
+                    React.createElement('li', null, restaurants[2].reviews[0].text),
+                    React.createElement('li', null, restaurants[3].reviews[0].text),
+                    React.createElement('li', null, restaurants[3].reviews[1].text),
+                )
+
+            )
+        }
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
