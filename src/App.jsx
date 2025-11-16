be17484870bf3972bdbf1f44c3908d8e9485ad4f
Denis <kolesnichenko-den@mail.ru>
2025-11-16 22:36:16 +0300

    feat: add some styles for application

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,7 +7,6 @@ import {restaurants} from './constants/mock.js';
 import {RestaurantPage} from "./pages/restaurantPage/restaurantPage.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -20,18 +19,6 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
